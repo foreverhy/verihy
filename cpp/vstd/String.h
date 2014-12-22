bd49bc21f6c7395f699788eefc5f88f77e9bdb32
Han Yao <hanyao@outlook.com>
2014-12-22 21:16:04 +0800

    draft:vstd::String.h

---
+++ cpp/vstd/String.h
@@ -0,0 +1,22 @@
+#ifndef VERIHY_VSTD_STRING_H
+#define VERIHY_VSTD_STRING_H
+
+#include <cstdint>
+
+namespace verihy{
+
+namespace vstd{
+
+class String{
+    typedef std::uint32_t size_type;
+  private:
+    char* _data;
+
+
+};
+
+}// namespace verihy::vstd
+
+}// namespace verihy
+
+#endif
